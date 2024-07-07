85dc2b50563d4acd16e7c4f6e77ba2091f75ebf2
Alam Guardin <alamguardin@gmail.com>
2024-07-07 12:57:52 -0500

    feat: add push method to linked list object

--- src/structures/linkedList.js
+++ src/structures/linkedList.js
@@ -1,10 +1,34 @@
 class Node {
-    constructor(data) {
+    constructor(data, prev = null) {
         this.data = data
         this.next = null
-        this.prev = null
+        this.prev = prev
     }
 }
 
-const node = new Node(1)
-console.log(node)
+class LinkedList {
+    constructor() {
+        this.head = null
+        this.tail = null
+    }
+
+    push(data) {
+        const newNode = new Node(data, this.tail)
+
+        if (!this.head) {
+            this.head = newNode
+        } else {
+            this.tail.next = newNode
+        }
+
+        this.tail = newNode
+    }
+}
+
+const linkedList = new LinkedList()
+linkedList.push(24)
+linkedList.push(43)
+linkedList.push(12)
+linkedList.push(15)
+
+console.log(linkedList.head)
