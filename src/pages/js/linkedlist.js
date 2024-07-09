2284c55d3b17b4b999c54e4c9576a1efccfbad3e
Alam Guardin <alamguardin@gmail.com>
2024-07-08 21:53:09 -0500

    refactor: Simplify event code and improve data visualization.

--- src/pages/js/linkedlist.js
+++ src/pages/js/linkedlist.js
@@ -1,52 +1,91 @@
 import '../../../style.css'
 import LinkedList from '../../structures/linkedList.js'
 
+/* Templates string */
+const ActionsNav = () => {
+    return (
+    `
+    <div class="actions">
+        <input type="text" class="actions-input" id="actions-input"/>
+        <button class="actions-button" id="actions-push">Push</button>
+        <button class="actions-button" id="actions-unshift">Unshift</button>
+        <button class="actions-button" id="actions-shift">Shift</button>
+        <button class="actions-button" id="actions-pop">Pop</button>
+    </div>
+    `
+    )
+}
+
+const Table = (content = '') => {
+    return (
+    `
+    <table>
+        <tr>
+            <th>Prev</th>
+            <th>Value</th>
+            <th>Next</th>
+        </tr>
+        ${content}
+    </table>
+    `
+    )
+}
+
 document.querySelector('#app').innerHTML = 
 `
-<div class="actions">
-    <input type="number" id="input-control"/>
-    <button id="btn-push">Push</button>
-    <button id="btn-unshift">Unshift</button>
-    <button id="btn-shift">Shift</button>
-    <button id="btn-pop">Pop</button>
-
-    <div id="data"><div>
-</div>
+${ActionsNav()}
+<div id="data">
+    ${Table()}
+<div>
 `
 
 const numbers = new LinkedList()
 
-document.querySelector('#btn-push').addEventListener('click', () => {
-    const newValue = document.querySelector('#input-control').value
-    numbers.push(Number(newValue))
-    document.querySelector('#input-control').value = ''
+const inputControl = document.querySelector('#actions-input')
+const pushButton = document.querySelector('#actions-push')
+const unshiftButton = document.querySelector('#actions-unshift')
+const shiftButton = document.querySelector('#actions-shift')
+const popButton = document.querySelector('#actions-pop')
+
+pushButton.addEventListener('click', () => {
+    if (!inputControl.value) { return null }
+
+    numbers.push(inputControl.value)
+    inputControl.value = ''
     showData()
 })
 
-document.querySelector('#btn-unshift').addEventListener('click', () => {
-    const newValue = document.querySelector('#input-control').value
-    numbers.unshift(Number(newValue))
-    document.querySelector('#input-control').value = ''
+unshiftButton.addEventListener('click', () => {
+    if (!inputControl.value) { return null }
+
+    numbers.unshift(inputControl.value)
+    inputControl.value = ''
     showData()
 })
 
-document.querySelector('#btn-shift').addEventListener('click', () => {
+shiftButton.addEventListener('click', () => {
     numbers.shift()
     showData()
 })
 
-document.querySelector('#btn-pop').addEventListener('click', () => {
+popButton.addEventListener('click', () => {
     numbers.pop()
     showData()
 })
+
 
 function getDataFromLinkedList(linkedlist) {
     let arr = []
     let current = linkedlist
 
     while(current) {
-        arr.push(current.data)
-        console.log(current.data)
+        const nodeObject = {
+            prev: current.prev ? current.prev.data : null,
+            value: current.data,
+            next: current.next ? current.next.data : null
+        }
+
+        arr.push(nodeObject)
         current = current.next
     }
 
@@ -58,9 +97,16 @@ function showData() {
     console.log(data)
     let fragment = '';
 
-    for(let item of data) {
-        fragment += `<div>${item}</div>`
+    for(let item in data) {
+        console.log(item)
+        fragment += 
+        `<tr>
+            <td>${data[item].prev}</td>
+            <td>${data[item].value}</td>
+            <td>${data[item].next}</td>
+        </tr>
+        `
     }
 
-    document.querySelector('#data').innerHTML = fragment
+    document.querySelector('#data').innerHTML = Table(fragment)
 }
